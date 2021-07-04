868c0623421ef9ed26b458652c55f36a6e50a0cf
Aleksei Zolotov <freopen@freopen.org>
2021-07-04 19:53:40 +0200

    Implement complex parts of User

--- src/server/data.rs
+++ src/server/data.rs
@@ -1,6 +1,8 @@
+use anyhow::Context as _;
 use async_graphql::{
   extensions::{ApolloTracing, Logger},
-  Context, EmptyMutation, EmptySubscription, Enum, Object, Schema, SimpleObject,
+  ComplexObject, Context, EmptyMutation, EmptySubscription, Enum, Object, Result, Schema,
+  SimpleObject,
 };
 use chrono::{DateTime, TimeZone, Utc};
 use derivative::Derivative;
@@ -23,6 +25,11 @@ macro_rules! derive_enum {
   };
 }
 
+async fn get_user(id: &str, ctx: &Context<'_>) -> Result<Option<User>> {
+  let db: &Db = ctx.data_unchecked();
+  Ok(db.users.find_one(doc! {"_id": id}, None).await?)
+}
+
 derive_struct! {
 pub struct Meta {
   pub last_processed_archive: String,
@@ -30,6 +37,7 @@ pub struct Meta {
 }
 
 derive_struct! {
+#[graphql(complex)]
 pub struct User {
   pub _id: String,
   #[derivative(Default(value="Utc.timestamp(0, 0)"))]
@@ -39,7 +47,20 @@ pub struct User {
 }
 }
 
+#[ComplexObject]
+impl User {
+  async fn erdos_link_for_number(&self, erdos_number: i32) -> Result<&ErdosLink> {
+    Ok(
+      &self.erdos_links[self
+        .erdos_links
+        .binary_search_by_key(&erdos_number, |erdos_link| erdos_link.erdos_number)
+        .map_err(|_| "Link with specified Erdos Number not found")?],
+    )
+  }
+}
+
 derive_struct! {
+#[graphql(complex)]
 pub struct ErdosLink {
   pub erdos_number: i32,
   #[derivative(Default(value="Utc.timestamp(0, 0)"))]
@@ -55,6 +76,17 @@ pub struct ErdosLink {
   pub moves_count: i32,
   pub winner_is_white: bool,
 }
+}
+
+#[ComplexObject]
+impl ErdosLink {
+  async fn loser(&self, ctx: &Context<'_>) -> Result<User> {
+    Ok(
+      get_user(&self.loser_id, ctx)
+        .await?
+        .context("Internal error: Link loser is not found in DB")?,
+    )
+  }
 }
 
 derive_struct! {
@@ -87,13 +119,8 @@ pub struct Query;
 
 #[Object]
 impl Query {
-  async fn user(&self, ctx: &Context<'_>, id: String) -> async_graphql::Result<User> {
-    let db: &Db = ctx.data()?;
-    if let Some(user) = db.users.find_one(doc! {"_id": id}, None).await? {
-      Ok(user)
-    } else {
-      Err(async_graphql::Error::new("Not found"))
-    }
+  async fn user(&self, ctx: &Context<'_>, id: String) -> Result<User> {
+    Ok(get_user(&id, ctx).await?.context("User not found")?)
   }
 }
 
